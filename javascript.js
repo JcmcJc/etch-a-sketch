362ca30e323adff88cbba06c1ea259ca3e5da9f9
Jonmarc <jjonmarc.jj@gmail.com>
2026-05-14 14:38:11 -0400

    The grid is dynamic
    It can be changed to the number of rows and columns specified by the function gridCreation("size")

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 const board = document.querySelector("#board");
 const grid = document.createElement("div");
-grid.style.border = "2px solid black";
 grid.classList.add("grid");
 
 board.appendChild(grid);
@@ -10,8 +9,18 @@ gridSquare.textContent = "test";
 gridSquare.style.border = "0.5px solid black";
 gridSquare.classList.add("grid-square");
 
-for (let i = 0; i < 16; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.classList.add("grid-square");
-  grid.appendChild(gridSquare);
+//Creating the grid squares based of the size demanded
+function gridCreation(size) {
+  let calc = 100 / size;
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.classList.add("grid-square");
+      grid.appendChild(gridSquare);
+      gridSquare.style.flex = `1 0 calc(${calc}%)`;
+    }
+  }
 }
+
+gridCreation(4);
